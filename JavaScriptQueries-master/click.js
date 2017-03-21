ab808dadd373cd922bdd34213392a63a45f85bec
kev2018 <8kludwig@stpatrick.org>
2017-03-21 13:58:42 -0500

    Add files via upload

---
+++ JavaScriptQueries-master/click.js
@@ -0,0 +1,9 @@
+//declare samecolor() here
+function samecolor(){
+    const doc = document.querySelectorAll("#samecolor div");
+for(let c =0;c<#samecolor>div.length;c++){
+        #samecolor>div[c].style.backgroundColor = #f00;
+    }
+
+}
+//declare diffcolor() here
