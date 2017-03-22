b6c958329595fb2e1340fd0a1b6043eb601ecdb0
kev2018 <8kludwig@stpatrick.org>
2017-03-22 14:03:21 -0500

    b1 b2 done

--- JavaScriptQueries-master/click.js
+++ JavaScriptQueries-master/click.js
@@ -1,9 +1,19 @@
 //declare samecolor() here
 function samecolor(){
     const doc = document.querySelectorAll("#samecolor div");
-for(let c =0;c<#samecolor>div.length;c++){
-        #samecolor>div[c].style.backgroundColor = #f00;
-    }
-
+      for(let c =0;c<doc.length;c++){
+        doc[c].style.backgroundColor = "#f00";
+      }
 }
 //declare diffcolor() here
+function diffcolor(){
+  const doc = document.querySelectorAll("diffcolor div");
+for(let k =0;k<doc.length;k++){
+  if([k]%2 == 1){
+      doc[k].style.backgroundColor = "#0f0";
+    }
+    else{
+      doc[k].style.backgroundColor = "#00f";
+    }
+  }
+}
