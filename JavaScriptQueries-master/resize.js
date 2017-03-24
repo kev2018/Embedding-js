92c23f7c974d6376307c9bbcb252e68d13ef13ad
kev2018 <8kludwig@stpatrick.org>
2017-03-24 13:56:38 -0500

    done

--- JavaScriptQueries-master/resize.js
+++ JavaScriptQueries-master/resize.js
@@ -1,4 +1,16 @@
 //Declare tabletSize() here
-
+function tabletSize(){
+    const doc = document.querySelectorAll("#samecolor div");
+    const wind = window.getComputerStyle(doc,null).getPropertyValue("height");
+    document.
+}
 
 //Declare phoneSize() here
+function phoneSize(){
+
+}
+function tabletSize(){
+    const doc = document.querySelectorAll("#samecolor div");
+    const wind = window.querySelectorAll("#samecolor div");
+    wind.resizeTo(270,600);
+}
